b7df221ccd2e9c571dfe3366e60c0747fb2054f1
saqo-torosyan <c-storosyan@contentanalyticsinc.com>
2019-12-10 20:14:28 +0400

    crm_system

--- connection.js
+++ connection.js
@@ -2,22 +2,22 @@ const mysql = require("mysql2");
 
 // mysql --host=us-cdbr-iron-east-05.cleardb.net --user=b29064450352db --password=d2f8acf6 --reconnect heroku_93409708d599b92
 
-const connection = mysql.createPool({
+/*const connection = mysql.createPool({
   connectionLimit: 5,
   host: "us-cdbr-iron-east-05.cleardb.net",
   user: "b29064450352db",
   database: "heroku_93409708d599b92",
   password: "d2f8acf6",
   multipleStatements: true
-});
+});*/
 
-/*const pool = mysql.createPool({
+const connection = mysql.createPool({
   connectionLimit: 5,
   host: "localhost",
   user: "root",
   database: "crm",
   password: "123456",
   multipleStatements: true
-});*/
+});
 
 module.exports = connection;
--- routes/users.js
+++ routes/users.js
@@ -1,97 +1,124 @@
-const express    = require("express");
-const Router     = express.Router();
-const DB         = require("../connection");
-const bodyParser = require("body-parser");
+const express      = require("express");
+const Router       = express.Router();
+const DB           = require("../connection");
+const bodyParser   = require("body-parser");
+const passwordHash = require('password-hash');
+const bcrypt       = require('bcrypt');
 
+const saltRounds       = 10;
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const TABLE            = 'users';
 
 Router.get("/", (req, res) => {
-  const { have_service } = req.body;
-  let queryStr = "select u.*, s.* from users as u left join users_to_services as us on u.id = us.user_id left join services as s on s.id = us.service_id";
+    const { have_service } = req.body;
+    let queryStr = "select u.*, s.* from " + TABLE + " as u left join users_to_services as us on u.id = us.user_id left join services as s on s.id = us.service_id";
 
-  if (have_service) {
-    queryStr =
-      "select u.*, s.* from users as u join users_to_services as us on u.id = us.user_id join services as s on s.id = us.service_id";
-  } else {
-    queryStr =
-      "select u.*, s.* from users as u left join users_to_services as us on u.id = us.user_id left join services as s on s.id = us.service_id";
-  }
-
-  DB.query(queryStr, (err, rows) => {
-    if (!err) {
-      res.send(rows);
+    if (have_service) {
+        queryStr = "select u.*, s.* from " + TABLE + " as u join users_to_services as us on u.id = us.user_id join services as s on s.id = us.service_id";
     } else {
-      console.log(err);
+        queryStr = "select u.*, s.* from " + TABLE + " as u left join users_to_services as us on u.id = us.user_id left join services as s on s.id = us.service_id";
     }
-  });
+
+    DB.query(queryStr, (err, rows) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log(err);
+        }
+    });
 });
 
-Router.post('/auth', function(req, res) {
-  var username = req.body.username;
-  var password = req.body.password;
+Router.post('/login', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
-  if (username && password) {
-    DB.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-      if (results.length > 0) {
-        req.session.loggedin = true;
-        req.session.username = username;
-        res.send('Loged in');
-      } else {
-        res.send('Incorrect Username and/or Password!');
-      }
-      res.end();
-    });
-  } else {
-    res.send('Please enter Username and Password!');
-    res.end();
-  }
+    if (username && password) {
+        DB.query(
+            "SELECT * FROM " + TABLE + " WHERE username = ?",
+            [username],
+            function(error, results, fields) {
+                if (results.length > 0) {
+                    bcrypt.compare(password, results[0].password, function (err, result) {
+                        if (result == true) {
+                            req.session.loggedin = true;
+                            req.session.username = username;
+                            res.send('Loged in');
+                            return
+                        } else {
+                            res.send('Incorrect Password!');
+                        }
+                    });
+                } else {
+                    res.send('Incorrect Username');
+                }
+            }
+        );
+    } else {
+        res.send('Please enter Username and Password!');
+        res.end();
+    }
 });
 
 Router.post("/", urlencodedParser, (req, res) => {
-  if (!req.body) return res.sendStatus(400);
+    if (!req.body) return res.sendStatus(400);
 
-  const { firstName, lastName, email, username, password } = req.body;
-  console.log("TCL: req.body", req.query);
-  DB.query(
-    "INSERT INTO users (firstName, lastName, email, username, password) VALUES (?,?,?,?,?)",
-    [firstName, lastName, email, username, password],
-    (err, rows, fields) => {
-      if (!err) {
-        res.send(rows);
-      } else {
-        console.log(err);
-      }
-    }
-  );
+    const { firstName, lastName, email, username, password } = req.body;
+
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            // Store hash in your password DB.
+            DB.query(
+                "INSERT INTO " + TABLE + " (firstName, lastName, email, username, password) VALUES (?,?,?,?,?)",
+                [firstName, lastName, email, username, hash],
+                (err, rows, fields) => {
+                    if (!err) {
+                        res.send(rows);
+                    } else {
+                        console.log(err);
+                    }
+
+                    res.end();
+                }
+            );
+        });
+    })
 });
 
 Router.put("/:id", urlencodedParser, function(req, res) {
-  if (!req.body) return res.sendStatus(400);
-  const { firstName } = req.body;
-  const id = req.params.id;
-  DB.query(
-    "UPDATE users SET firstName=? WHERE id=?",
-    [firstName, id],
-    function(err, result) {
-      if (err) return console.log(err);
-      res.send(result);
-    }
-  );
+    if (!req.body) return res.sendStatus(400);
+
+    const { firstName } = req.body;
+    const id = req.params.id;
+
+    DB.query(
+        "UPDATE " + TABLE + " SET firstName=? WHERE id=?",
+        [firstName, id],
+        function(err, result) {
+            if (err) return console.log(err);
+            res.send(result);
+        }
+    );
 });
 
 Router.delete("/:id", function(req, res) {
-  const id = req.params.id;
-  DB.query("DELETE FROM users WHERE id=?", [id], function(err, result) {
-    if (res)
-      DB.query(
-        "DELETE FROM users_to_services WHERE user_id=?",
+    const id = req.params.id;
+
+    DB.query(
+        "DELETE FROM " + TABLE + " WHERE id=?",
         [id],
         function(err, result) {
-          res.send(result);
+            if (res)
+                DB.query(
+                    "DELETE FROM users_to_services WHERE user_id=?",
+                    [id],
+                    function(err, result) {
+                        res.send(result);
+                    }
+                );
+
+                res.send(result);
         }
-      );
-    res.send(result);
-  });
+    );
 });
 
 module.exports = Router;
